5fd54e980a004e44aed6cfc122e4bef13ef54001
chloe0holland <chloeh2018@banks.k12.or.us>
2017-11-19 15:03:15 -0800

    update to tournament changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AutonomousDriver.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AutonomousDriver.java
@@ -12,6 +12,8 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 public class AutonomousDriver extends LinearOpMode {
 
     boolean runLinearCode = true;
+    int teamColor;
+    String vuforiaPosition = "unknown";
 
     @Override
     public void runOpMode() {
@@ -24,8 +26,46 @@ public class AutonomousDriver extends LinearOpMode {
 
             if (runLinearCode) {
 
-                if (al.robot.isRed == 1){
+                teamColor = al.setTeamColor();
+                al.decipherJewelAndKnockOff(telemetry, this);
+                al.robot.jewelActuatorServo.setPosition(0.3);
+                // al.driveAtAngle(18, 135, telemetry, this);// switch to unit circle
+                telemetry.addData("team color = ", al.robot.isRed);
+                telemetry.addData("Rev Blue", al.robot.colorSensorREV.blue());
+                telemetry.addData("Rev Red", al.robot.colorSensorREV.red());
+                telemetry.update();
+                if (teamColor == 1) {
+                    al.driveAtAngle(25, 120, telemetry, this);
+                } else {
+                    al.driveAtAngle(25, 30, telemetry, this);
+                }
 
+
+
+
+
+
+
+             //   al.driveAtAngle(18, 270, telemetry, this);
+                //al.decipherJewelAndKnockOff(telemetry, this);
+                //al.driveAtAngle(18, 270, telemetry, this);// switch to unit circle
+
+                /*
+                //al.decipherJewelAndKnockOff(telemetry, this);
+                if (al.robot.isRed == 1){
+                    telemetry.addLine("I am on the red team! :D");
+                    telemetry.update();
+                    //al.driveAtAngle();
+
+                } else if (al.robot.isRed == 0){
+                    telemetry.addLine("I am on the blue team! :D");
+                    telemetry.update();
+                    //al.driveAtAngle();
+
+                } else {
+                    telemetry.addLine("I had troubles communicating with my color sensor and I don't know what team I am on");
+                    telemetry.update();
+                    //wouldn't it be so cool to add LEDs here later in the season where it would flash for errors
                 }
 
                 /*telemetry.addLine("First drive");
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AutonomousDriverTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AutonomousDriverTest.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode.Autonomous;
 
+import android.graphics.Color;
+
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
@@ -17,30 +19,45 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 public class AutonomousDriverTest extends LinearOpMode {
 
     boolean runLinearCode = true;
-
+    int teamColor = 0;
+    String vuforiaPosition = "unknown";
 
     @Override
     public void runOpMode () throws InterruptedException {
 
         AutonomousLibrary al = new AutonomousLibrary();
         al.init(hardwareMap);
+        double SCALE_FACTOR = 255;
+        // hsvValues is an array that will hold the hue, saturation, and value information.
+        float hsvValues[] = {0F, 0F, 0F};
+        // values is a reference to the hsvValues array.
+        final float values[] = hsvValues;
         waitForStart();
 
         while (opModeIsActive()) {
+            Color.RGBToHSV((int) (al.robot.colorSensorREV.red() * SCALE_FACTOR),
+                    (int) (al.robot.colorSensorREV.green() * SCALE_FACTOR),
+                    (int) (al.robot.colorSensorREV.blue() * SCALE_FACTOR),
+                    hsvValues);
 
             if (runLinearCode) {
 
-                telemetry.addData("team color = ", al.robot.isRed);
+
+
+                teamColor = al.setTeamColor();
                 al.decipherJewelAndKnockOff(telemetry, this);
-                //al.driveAtAngle(18, 135, telemetry, this);// switch to unit circle
-
-/*
-                telemetry.addLine("Vuforia");
-                telemetry.update();
-                al.pictoDecipher(telemetry, this);
-                telemetry.addLine("2nd drive");
-                telemetry.update();*/
-
+                al.robot.jewelActuatorServo.setPosition(0.3);
+                vuforiaPosition = al.pictoDecipher(telemetry, this);
+                // al.driveAtAngle(18, 135, telemetry, this);// switch to unit circle
+                al.closeArms();
+                if (teamColor == 1) {
+                    al.driveAtAngle(25, 90, telemetry, this);
+                } else {
+                    al.driveAtAngle(25, 30, telemetry, this);
+                }
+                al.driveToVuforiaPositionFromTheLeft(telemetry, this, vuforiaPosition);
+                al.driveAtAngle(4, 90, telemetry, this);
+                al.openArms();
             }
 
             runLinearCode = false;
