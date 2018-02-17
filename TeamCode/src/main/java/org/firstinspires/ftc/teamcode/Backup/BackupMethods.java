177a2c25b47e74bcb32b7ab81cbbb27a8ff5264e
chloe0holland <chloeh2018@banks.k12.or.us>
2018-02-17 15:19:15 -0800

    Add backups

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Backup/BackupMethods.java
@@ -0,0 +1,34 @@
+package org.firstinspires.ftc.teamcode.Backup;
+
+import android.graphics.Color;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.navigation.RelicRecoveryVuMark;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
+import org.firstinspires.ftc.teamcode.Common.CommonLibrary;
+import org.firstinspires.ftc.teamcode.Common.RobotHardware;
+
+import java.util.Locale;
+
+
+/**
+ * Created by Robotics on 8/27/2017.
+ */
+public class BackupMethods {
+    
+}
