42bcbefbc2167e1bef00f9ef6e35428621c186e5
chloe0holland <chloeh2018@banks.k12.or.us>
2018-02-17 15:23:07 -0800

    Set up testing dir

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/TestDriver.java
@@ -0,0 +1,47 @@
+package org.firstinspires.ftc.teamcode.Testing;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.Common.CommonLibrary;
+import org.firstinspires.ftc.teamcode.TeleOp.TeleOpLibrary;
+
+/**
+ * Created by Robotics on 8/27/2017.
+ */
+
+@TeleOp(name = "TeleOp")
+//@Disabled
+public class TestDriver extends OpMode {
+
+    @Override
+    public void init() {
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
+     */
+    @Override
+    public void init_loop() {
+    }
+
+    /*
+     * Code to run ONCE when the driver hits PLAY
+     */
+    @Override
+    public void start() {
+
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
+     */
+    @Override
+    public void loop() {
+
+    }
+
+    @Override
+    public void stop() {
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/TestLibrary.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode.Testing;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.teamcode.Common.CommonLibrary;
+import org.firstinspires.ftc.teamcode.Common.RobotHardware;
+import org.firstinspires.ftc.teamcode.TeleOp.TeleOpDriver;
+import org.firstinspires.ftc.teamcode.TeleOp.TeleOpDriverTest;
+
+/**
+ * Created by Robotics on 8/27/2017.
+ */
+public class TestLibrary {
+
+
+    public void init(OpMode caller) {
+
+    }
+
+}
